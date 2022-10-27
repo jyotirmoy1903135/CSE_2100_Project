c4d7e2a03aff966db14141a504b5e6fb5d743576
jyotirmoy1903135 <70074815+jyotirmoy1903135@users.noreply.github.com>
2022-10-27 15:35:15 +0600

    Update Opening_Youtube.py

--- Opening_Youtube.py
+++ Opening_Youtube.py
@@ -1,7 +1,9 @@
-        import webbrowser
-        speech = ("Input command: ")
-        speech = speech.lower()
-        if "open youtube" in speech:
-            speak("Opening youtube, sir")
-            time.sleep(0.5)
-            webbrowser.open("youtube.com")
+import webbrowser
+import time
+
+speech = input("Input command: ")
+speech = speech.lower()
+if "open youtube" in speech:
+    print("Opening youtube, sir")
+    time.sleep(0.5)
+    webbrowser.open("youtube.com")
