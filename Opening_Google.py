b45adcb5036de049f6777f230254e041494d4ac3
jyotirmoy1903135 <70074815+jyotirmoy1903135@users.noreply.github.com>
2022-10-27 15:34:30 +0600

    Update Opening_Google.py

--- Opening_Google.py
+++ Opening_Google.py
@@ -1,7 +1,9 @@
-        import webbrowser
-        speech = ("Input command: ")
-        speech = speech.lower()
-        if "open google" in speech:
-            speak("Opening google, sir")
-            time.sleep(0.5)
-            webbrowser.open("google.com")
+import webbrowser
+import time
+
+speech = input("Input command: ")
+speech = speech.lower()
+if "open google" in speech:
+    print("Opening google, sir")
+    time.sleep(0.5)
+    webbrowser.open("google.com")
